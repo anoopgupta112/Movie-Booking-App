e08fcc86092d454edd7ed6bb768ac54cd9b0ec10
anoopgupta112 <anoopgupta112>
2022-05-15 21:42:25 +0530

    Checkpoint 5

---
+++ models/user.model.js
@@ -0,0 +1,45 @@
+
+module.exports = (mongoose) => {
+    const userSchema = mongoose.Schema({
+        userid: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+
+        },
+        first_name: {
+            type: String,
+            required: true,
+        },
+        last_name: {
+            type: String,
+            required: true,
+        },
+        username: String,
+        contact: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            enum: ["user", "admin"],
+            default: "user",
+        },
+        isLoggedIn: Boolean,
+        uuid: String,
+        accesstoken: String,
+        coupens: [],
+        bookingRequests: [],
+    });
+
+
+    return userSchema;
+};
--- server.js
+++ server.js
@@ -18,3 +18,19 @@ app.get("/artists", (request, resopnse) => {
 app.listen(PORT, () => {
     console.log("Running at PORT " + PORT)
 })
+
+// database code
+const db = require("./models");
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database!");
+
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
